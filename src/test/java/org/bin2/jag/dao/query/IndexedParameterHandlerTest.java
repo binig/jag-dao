5c370916a0af2d11b49c53c4c37620a3717d217c
benoit.roger@gmail.com <benoit.roger@gmail.com@9162598b-051d-55d4-be8b-cbc35df51b04>
2012-02-12 10:44:10 +0000

    add a test for indexedParameterHandler and some package doc

---
+++ src/test/java/org/bin2/jag/dao/query/IndexedParameterHandlerTest.java
@@ -0,0 +1,29 @@
+package org.bin2.jag.dao.query;
+
+import org.hibernate.Query;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.Test;
+
+/**
+ * @author broger
+ *         Date: 12/02/12
+ *         Time: 11:35
+ */
+public class IndexedParameterHandlerTest {
+
+    @Test
+    public void testIndexedParameterHandlerTest() {
+        final int idx = 10;
+
+        final String param = "Param";
+        IndexedParameterHandler h = new IndexedParameterHandler(idx);
+        Mockery ctx = new Mockery();
+        final Query query = ctx.mock(Query.class);
+        ctx.checking(new Expectations() {{
+            one(query).setParameter(idx, param);
+        }});
+        h.proceedParameter(query, param);
+        ctx.assertIsSatisfied();
+    }
+}
